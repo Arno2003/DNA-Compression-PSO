63c5de8515267c2bd8574faa5214abc4022a9979
Arno2003 <arnabkarate@gmail.com>
2024-07-20 08:30:22 -0400

    ennaf error solved

--- Code/naf/ennaf/src/ennaf.c
+++ Code/naf/ennaf/src/ennaf.c
@@ -11,7 +11,6 @@
 #include "platform.h"
 #include "encoders.h"
 #include "tables.c"
-#include "
 
 #define UNCOMPRESSED_BUFFER_SIZE (1ull * 1000 * 1000)
 #define COMPRESSED_BUFFER_SIZE (2ull * 1000 * 1000)
